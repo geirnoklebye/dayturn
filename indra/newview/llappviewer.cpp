5b4fcbcfa2995c83964b2beacd2c0d83e13cc915
Dave Parks <davep@lindenlab.com>
2024-07-09 15:54:18 -0500

    #1943 make sys free in texture console llmemorysavailphysicalmeminkb tell the truth (#1966)
    * Also fix for crash when applying MoaP to PBR material

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -337,7 +337,6 @@ bool			gUseWireframe = false;
 //use for remember deferred mode in wireframe switch
 bool			gInitialDeferredModeForWireframe = false;
 
-LLMemoryInfo gSysMemory;
 U64Bytes gMemoryAllocated(0); // updated in display_stats() in llviewerdisplay.cpp
 
 std::string gLastVersionChannel;
