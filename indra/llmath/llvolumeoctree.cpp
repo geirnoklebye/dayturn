11dc929f6e40f045419ab6fbbf93fa8c11bdc857
Geir Nøklebye <geir.noklebye@dayturn.com>
2024-10-26 11:22:32 +0200

    DAYTW-40 nullptr cosmetics in llmath

--- indra/llmath/llvolumeoctree.cpp
+++ indra/llmath/llvolumeoctree.cpp
@@ -140,7 +140,7 @@ void LLOctreeTriangleRayIntersect::visit(const LLOctreeNode<LLVolumeTriangle, LL
 				*mClosestT = t;
 				mHitFace = true;
 
-				if (mIntersection != NULL)
+				if (mIntersection != nullptr)
 				{
 					LLVector4a intersect = mDir;
 					intersect.mul(*mClosestT);
@@ -152,7 +152,7 @@ void LLOctreeTriangleRayIntersect::visit(const LLOctreeNode<LLVolumeTriangle, LL
 				U32 idx1 = tri->mIndex[1];
 				U32 idx2 = tri->mIndex[2];
 
-				if (mTexCoord != NULL)
+				if (mTexCoord != nullptr)
 				{
 					LLVector2* tc = (LLVector2*) mFace->mTexCoords;
 					*mTexCoord = ((1.f - a - b)  * tc[idx0] +
@@ -161,7 +161,7 @@ void LLOctreeTriangleRayIntersect::visit(const LLOctreeNode<LLVolumeTriangle, LL
 
 				}
 
-				if (mNormal != NULL)
+				if (mNormal != nullptr)
 				{
 					LLVector4a* norm = mFace->mNormals;
 								
@@ -181,7 +181,7 @@ void LLOctreeTriangleRayIntersect::visit(const LLOctreeNode<LLVolumeTriangle, LL
 					*mNormal		= n1; 
 				}
 
-				if (mTangent != NULL)
+				if (mTangent != nullptr)
 				{
 					LLVector4a* tangents = mFace->mTangents;
 								
