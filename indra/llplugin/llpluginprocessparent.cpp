mayatonton/phoenix-firestorm

macOS 26 build changes (#4239)
* Fix for VLAs now emitting warnings on Xcode 26, and AGL being removed from the SDK.

Note: there was no warning or notice provided for AGL's removal.

* Whitespace.

indra/llplugin/llpluginprocessparent.cpp
@@ -575,7 +575,7 @@ void LLPluginProcessParent::idle(void)
                             params.args.add("-e");
                             params.args.add("tell application \"Terminal\"");
                             params.args.add("-e");
-                            params.args.add(STRINGIZE("set win to do script \"lldb -pid "
+                            params.args.add(STRINGIZE("set win to do script \"lldb -p "
                                                       << mProcess->getProcessID() << "\""));
                             params.args.add("-e");
                             params.args.add("do script \"continue\" in win");

geirnoklebye/dayturn

indra/llplugin/llpluginprocessparent.cpp
@@ -599,7 +599,7 @@ void LLPluginProcessParent::idle(void)
 						    params.args.add("-e");
 						    params.args.add("tell application \"Terminal\"");
 						    params.args.add("-e");
-						    params.args.add(STRINGIZE("set win to do script \"gdb -pid "
+						    params.args.add(STRINGIZE("set win to do script \"gdb -p "
 												      << mProcess->getProcessID() << "\""));
 						    params.args.add("-e");
 						    params.args.add("do script \"continue\" in win");