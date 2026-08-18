geirnoklebye/dayturn

commit d53e5c82c0a980d1b6cd6aba2466bf0aa60f944d

Add a new static function LLFile::size() to determine the size of a file_name. Replace LLAPRFile::remove(), LLAPRFile::size() and LLAPRFile::isExist() with according functions from LLFile and retire these LLAPRFile methods and the never used LLAPRFile::rename(), LLAPRFile::makeDir() and LLAPRFile::removeDir() functions. Also clean up remarks about the threading safety of the APRCachePool, which is not used in these locations anymore.

indra/newview/llappviewer.cpp
@@ -757,10 +757,10 @@ bool LLAppViewer::init()
 
 	std::string clear_settings_filename = gDirUtilp->getExpandedFilename(LL_PATH_LOGS,"CLEAR");
 	std::string clear_color_settings_filename = gDirUtilp->getExpandedFilename(LL_PATH_LOGS,"CLEAR_COLOR");
-	LLAPRFile clear_file ;
-	mPurgeSettings = (clear_file.isExist(clear_settings_filename));
-	LLAPRFile clear_color_file;
-	bool clear_colors = (clear_color_file.isExist(clear_color_settings_filename));
+	LLFile clear_file ;
+	mPurgeSettings = (clear_file.isfile(clear_settings_filename));
+	LLFile clear_color_file;
+	bool clear_colors = (clear_color_file.isfile(clear_color_settings_filename));
 	if (mPurgeSettings || clear_colors)
 	{
 		if (mPurgeSettings)
@@ -2391,7 +2391,7 @@ void errorCallback(LLError::ELevel level, const std::string &error_string)
         LLAppViewer::instance()->writeDebugInfo();
 
         std::string error_marker_file = gDirUtilp->getExpandedFilename(LL_PATH_LOGS, ERROR_MARKER_FILE_NAME);
-        if (!LLAPRFile::isExist(error_marker_file, nullptr, LL_APR_RB))
+        if (!LLFile::isfile(error_marker_file))
         {
             // If marker doesn't exist, create a marker with llerror code for next launch
             // otherwise don't override existing file
@@ -4109,7 +4109,7 @@ void LLAppViewer::processMarkerFiles()
 	bool marker_is_same_version = true;
 	// first, look for the marker created at startup and deleted on a clean exit
 	mMarkerFileName = gDirUtilp->getExpandedFilename(LL_PATH_LOGS,MARKER_FILE_NAME);
-	if (LLAPRFile::isExist(mMarkerFileName, nullptr, LL_APR_RB))
+    if (LLFile::isfile(mMarkerFileName))
 	{
 		// File exists...
 		// first, read it to see if it was created by the same version (we need this later)
@@ -4196,7 +4196,7 @@ void LLAppViewer::processMarkerFiles()
 	// check for any last exec event report based on whether or not it happened during logout
 	// (the logout marker is created when logout begins)
 	std::string logout_marker_file =  gDirUtilp->getExpandedFilename(LL_PATH_LOGS, LOGOUT_MARKER_FILE_NAME);
-	if(LLAPRFile::isExist(logout_marker_file, nullptr, LL_APR_RB))
+    if(LLFile::isfile(logout_marker_file))
 	{
 		if (markerIsSameVersion(logout_marker_file))
 		{
@@ -4207,11 +4207,11 @@ void LLAppViewer::processMarkerFiles()
 		{
 			LL_INFOS("MarkerFile") << "Logout crash marker '"<< logout_marker_file << "' found, but versions did not match" << LL_ENDL;
 		}
-		LLAPRFile::remove(logout_marker_file);
+        LLFile::remove(logout_marker_file);
 	}
 	// and last refine based on whether or not a marker created during a non-llerr crash is found
 	std::string error_marker_file = gDirUtilp->getExpandedFilename(LL_PATH_LOGS, ERROR_MARKER_FILE_NAME);
-	if(LLAPRFile::isExist(error_marker_file, nullptr, LL_APR_RB))
+	if(LLFile::isfile(error_marker_file))
 	{
         S32 marker_code = getMarkerErrorCode(error_marker_file);
         if (marker_code >= 0)
@@ -4236,7 +4236,7 @@ void LLAppViewer::processMarkerFiles()
 		{
 			LL_INFOS("MarkerFile") << "Error marker '"<< error_marker_file << "' marker found, but versions did not match" << LL_ENDL;
 		}
-		LLAPRFile::remove(error_marker_file);
+        LLFile::remove(error_marker_file);
 	}
 }
 
@@ -4247,7 +4247,7 @@ void LLAppViewer::removeMarkerFiles()
 		if (mMarkerFile.getFileHandle())
 		{
 			mMarkerFile.close() ;
-			LLAPRFile::remove( mMarkerFileName );
+            LLFile::remove( mLogoutMarkerFileName );
 			LL_DEBUGS("MarkerFile") << "removed exec marker '"<<mMarkerFileName<<"'"<< LL_ENDL;
 		}
 		else
@@ -4258,7 +4258,7 @@ void LLAppViewer::removeMarkerFiles()
 		if (mLogoutMarkerFile.getFileHandle())
 		{
 			mLogoutMarkerFile.close();
-			LLAPRFile::remove( mLogoutMarkerFileName );
+			LLFile::remove( mLogoutMarkerFileName );
 			LL_DEBUGS("MarkerFile") << "removed logout marker '"<<mLogoutMarkerFileName<<"'"<< LL_ENDL;
 		}
 		else
@@ -5642,6 +5642,10 @@ void LLAppViewer::createErrorMarker(eLastExecEvent error_code) const
             file.close();
         }
     }
+}bool LLAppViewer::errorMarkerExists() const
+{
+    std::string error_marker_file = gDirUtilp->getExpandedFilename(LL_PATH_LOGS, ERROR_MARKER_FILE_NAME);
+    return LLFile::isfile(error_marker_file);
 }
 
 void LLAppViewer::outOfMemorySoftQuit()

indra/newview/llvocache.cpp
@@ -1237,7 +1237,8 @@ void LLVOCache::removeFromCache(HeaderEntryInfo* entry)
 
 	std::string filename;
 	getObjectCacheFilename(entry->mHandle, filename);
-	LLAPRFile::remove(filename, mLocalAPRFilePoolp);
+    LL_WARNS("VOCache") << "Removing object cache for handle " << entry->mHandle << "Filename: " << filename << LL_ENDL;
+    LLFile::remove(filename);
 	entry->mTime = INVALID_TIME ;
 	updateEntry(entry) ; //update the head file.
 }
@@ -1254,7 +1255,7 @@ void LLVOCache::readCacheHeader()
 	clearCacheInMemory();	
 
 	bool success = true ;
-	if (LLAPRFile::isExist(mHeaderFileName, mLocalAPRFilePoolp))
+    if (LLFile::isfile(mHeaderFileName))
 	{
 		LLAPRFile apr_file(mHeaderFileName, APR_READ|APR_BINARY, mLocalAPRFilePoolp);		
 		

indra/llcommon/llapr.cpp
@@ -651,125 +651,6 @@ S32 LLAPRFile::writeEx(const std::string& filename, const void *buf, S32 offset,
 	return (S32)bytes_written;
 }
 
-//static
-bool LLAPRFile::remove(const std::string& filename, LLVolatileAPRPool* pool)
-{
-	apr_status_t s;
-
-	LLAPRFilePoolScope scope(pool);
-	s = apr_file_remove(filename.c_str(), scope.getVolatileAPRPool());
-
-	if (s != APR_SUCCESS)
-	{
-		ll_apr_warn_status(s);
-		LL_WARNS("APR") << " Attempting to remove filename: " << filename << LL_ENDL;
-		return false;
-	}
-	return true;
-}
-
-//static
-bool LLAPRFile::rename(const std::string& filename, const std::string& newname, LLVolatileAPRPool* pool)
-{
-	apr_status_t s;
-
-	LLAPRFilePoolScope scope(pool);
-	s = apr_file_rename(filename.c_str(), newname.c_str(), scope.getVolatileAPRPool());
-	
-	if (s != APR_SUCCESS)
-	{
-		ll_apr_warn_status(s);
-		LL_WARNS("APR") << " Attempting to rename filename: " << filename << LL_ENDL;
-		return false;
-	}
-	return true;
-}
-
-//static
-bool LLAPRFile::isExist(const std::string& filename, LLVolatileAPRPool* pool, apr_int32_t flags)
-{
-	apr_file_t* apr_file;
-	apr_status_t s;
-
-	LLAPRFilePoolScope scope(pool);
-	s = apr_file_open(&apr_file, filename.c_str(), flags, APR_OS_DEFAULT, scope.getVolatileAPRPool());	
-
-	if (s != APR_SUCCESS || !apr_file)
-	{
-		return false;
-	}
-	else
-	{
-		apr_file_close(apr_file) ;
-		return true;
-	}
-}
-
-//static
-S32 LLAPRFile::size(const std::string& filename, LLVolatileAPRPool* pool)
-{
-	apr_file_t* apr_file;
-	apr_finfo_t info;
-	apr_status_t s;
-
-	LLAPRFilePoolScope scope(pool);
-	s = apr_file_open(&apr_file, filename.c_str(), APR_READ, APR_OS_DEFAULT, scope.getVolatileAPRPool());
-	
-	if (s != APR_SUCCESS || !apr_file)
-	{				
-		return 0;
-	}
-	else
-	{
-		apr_status_t s = apr_file_info_get(&info, APR_FINFO_SIZE, apr_file);		
-
-		apr_file_close(apr_file) ;
-		
-		if (s == APR_SUCCESS)
-		{
-			return (S32)info.size;
-		}
-		else
-		{
-			return 0;
-		}
-	}
-}
-
-//static
-bool LLAPRFile::makeDir(const std::string& dirname, LLVolatileAPRPool* pool)
-{
-	apr_status_t s;
-
-	LLAPRFilePoolScope scope(pool);
-	s = apr_dir_make(dirname.c_str(), APR_FPROT_OS_DEFAULT, scope.getVolatileAPRPool());
-		
-	if (s != APR_SUCCESS)
-	{
-		ll_apr_warn_status(s);
-		LL_WARNS("APR") << " Attempting to make directory: " << dirname << LL_ENDL;
-		return false;
-	}
-	return true;
-}
-
-//static
-bool LLAPRFile::removeDir(const std::string& dirname, LLVolatileAPRPool* pool)
-{
-	apr_status_t s;
-
-	LLAPRFilePoolScope scope(pool);
-	s = apr_file_remove(dirname.c_str(), scope.getVolatileAPRPool());
-	
-	if (s != APR_SUCCESS)
-	{
-		ll_apr_warn_status(s);
-		LL_WARNS("APR") << " Attempting to remove directory: " << dirname << LL_ENDL;
-		return false;
-	}
-	return true;
-}
-
 //
 //end of static components of LLAPRFile
 //*******************************************************************************************************************************

indra/llcommon/llapr.h
@@ -185,14 +185,6 @@ private:
 	static apr_status_t close(apr_file_t* file) ;
 	static S32 seek(apr_file_t* file, apr_seek_where_t where, S32 offset);
 public:
-	// returns false if failure:
-	static bool remove(const std::string& filename, LLVolatileAPRPool* pool = nullptr);
-	static bool rename(const std::string& filename, const std::string& newname, LLVolatileAPRPool* pool = nullptr);
-	static bool isExist(const std::string& filename, LLVolatileAPRPool* pool = nullptr, apr_int32_t flags = APR_READ);
-	static S32 size(const std::string& filename, LLVolatileAPRPool* pool = nullptr);
-	static bool makeDir(const std::string& dirname, LLVolatileAPRPool* pool = nullptr);
-	static bool removeDir(const std::string& dirname, LLVolatileAPRPool* pool = nullptr);
-
 	// Returns bytes read/written, 0 if read/write fails:
 	static S32 readEx(const std::string& filename, void *buf, S32 offset, S32 nbytes, LLVolatileAPRPool* pool = nullptr);
 	static S32 writeEx(const std::string& filename, const void *buf, S32 offset, S32 nbytes, LLVolatileAPRPool* pool = nullptr); // offset<0 means append

indra/llcommon/llfile.cpp
@@ -498,6 +498,45 @@ int LLFile::stat(const std::string& filename, llstat* filestatus, int suppress_e
 }
 
 // static
+S64 LLFile::size(const std::string& filename, int suppress_error)
+{
+#if LL_WINDOWS
+    std::wstring utf16filename = utf8path_to_wstring(filename);
+    HANDLE file_handle = CreateFileW(utf16filename.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (INVALID_HANDLE_VALUE != file_handle)
+    {
+        LARGE_INTEGER file_size;
+        if (GetFileSizeEx(file_handle, &file_size))
+        {
+            CloseHandle(file_handle);
+            return file_size.QuadPart;
+        }
+    }
+    // Get last error before calling the CloseHandle() function
+    int rc = set_errno_from_oserror(GetLastError());
+    if (INVALID_HANDLE_VALUE != file_handle)
+    {
+        CloseHandle(file_handle);
+    }
+#else
+    llstat filestatus;
+    int rc = ::stat(filename.c_str(), &filestatus);
+    if (rc == 0)
+    {
+        if (S_ISREG(filestatus.st_mode))
+        {
+            return filestatus.st_size;
+        }
+        // We got something else than a file
+        rc = -1;
+        errno = EACCES;
+    }
+#endif
+    warnif("size", filename, rc, suppress_error);
+    return 0;
+}
+
+// static
 unsigned short LLFile::getattr(const std::string& filename, bool dontFollowSymLink, int suppress_error)
 {
 #if LL_WINDOWS

indra/llcommon/llfile.h
@@ -147,6 +147,13 @@ public:
     ///  @returns 0 on failure and a st_mode value with either S_IFDIR or S_IFREG set otherwise
     ///  together with the three access bits which under Windows only the write bit is relevant.
 
+    /// get the size of a file in bytes
+    static  S64     size(const std::string& filename, int suppress_error = ENOENT);
+    ///> returns the size of a file in bytes
+    /// we pass by default ENOENT in the optional 'suppress_error' parameter to not spam the log with
+    /// warnings when the file does not exist
+    /// @returns the file size on success and 0 on failure.
+
     /// check if filename is an existing directory
     static  bool    isdir(const std::string& filename);
     ///< @returns true if the path is for an existing directory

indra/newview/llappviewer.h
@@ -238,6 +238,7 @@ public:
 
     // Writes an error code into the error_marker file for use on next startup.
     void createErrorMarker(eLastExecEvent error_code) const;
+    bool errorMarkerExists() const;
 
     // Attempt a 'soft' quit with disconnect and saving of settings/cache.
     // Intended to be thread safe.

indra/newview/lltexturecache.cpp
@@ -183,7 +183,7 @@ private:
 
 bool LLTextureCacheLocalFileWorker::doRead()
 {
-	S32 local_size = LLAPRFile::size(mFileName, mCache->getLocalAPRFilePool());
+    S32 local_size = (S32)LLFile::size(mFileName);
 
 	if (local_size > 0 && mFileName.size() > 4)
 	{
@@ -343,7 +343,7 @@ bool LLTextureCacheRemoteWorker::doRead()
 		// Is it a JPEG2000 file? 
 		{
 			local_filename = filename + ".j2c";
-			local_size = LLAPRFile::size(local_filename, mCache->getLocalAPRFilePool());
+            local_size = (S32)LLFile::size(local_filename);
 			if (local_size > 0)
 			{
 				mImageFormat = IMG_CODEC_J2C;
@@ -353,7 +353,7 @@ bool LLTextureCacheRemoteWorker::doRead()
 		if (local_size == 0)
 		{
 			local_filename = filename + ".jpg";
-			local_size = LLAPRFile::size(local_filename, mCache->getLocalAPRFilePool());
+            local_size = (S32)LLFile::size(local_filename);
 			if (local_size > 0)
 			{
 				mImageFormat = IMG_CODEC_JPEG;
@@ -364,7 +364,7 @@ bool LLTextureCacheRemoteWorker::doRead()
 		if (local_size == 0)
 		{
 			local_filename = filename + ".tga";
-			local_size = LLAPRFile::size(local_filename, mCache->getLocalAPRFilePool());
+            local_size = (S32)LLFile::size(local_filename);
 			if (local_size > 0)
 			{
 				mImageFormat = IMG_CODEC_TGA;
@@ -493,7 +493,7 @@ bool LLTextureCacheRemoteWorker::doRead()
 	if (!done && (mState == BODY))
 	{
 		std::string filename = mCache->getTextureFileName(mID);
-		S32 filesize = LLAPRFile::size(filename, mCache->getLocalAPRFilePool());
+        S32 filesize = (S32)LLFile::size(filename);
 
 		if (filesize && (filesize + TEXTURE_CACHE_ENTRY_SIZE) > mOffset)
 		{
@@ -946,7 +946,7 @@ bool LLTextureCache::isInLocal(const LLUUID& id)
 	// Is it a JPEG2000 file? 
 	{
 		local_filename = filename + ".j2c";
-		local_size = LLAPRFile::size(local_filename, getLocalAPRFilePool());
+        local_size = (S32)LLFile::size(local_filename);
 		if (local_size > 0)
 		{
 			return true ;
@@ -956,7 +956,7 @@ bool LLTextureCache::isInLocal(const LLUUID& id)
 	// If not, is it a jpeg file?		
 	{
 		local_filename = filename + ".jpg";
-		local_size = LLAPRFile::size(local_filename, getLocalAPRFilePool());
+        local_size = (S32)LLFile::size(local_filename);
 		if (local_size > 0)
 		{
 			return true ;
@@ -966,7 +966,7 @@ bool LLTextureCache::isInLocal(const LLUUID& id)
 	// Hmm... What about a targa file? (used for UI texture mostly)		
 	{
 		local_filename = filename + ".tga";
-		local_size = LLAPRFile::size(local_filename, getLocalAPRFilePool());
+        local_size = (S32)LLFile::size(local_filename);
 		if (local_size > 0)
 		{
 			return true ;
@@ -1022,10 +1022,10 @@ void LLTextureCache::purgeCache(ELLPath location, bool remove_dir)
 		{
 			std::string file_name = gDirUtilp->getExpandedFilename(location, entries_filename);
 			// mHeaderAPRFilePoolp because we are under header mutex, and can be in main thread
-			LLAPRFile::remove(file_name, mHeaderAPRFilePoolp);
+            LLFile::remove(file_name);
 
 			file_name = gDirUtilp->getExpandedFilename(location, cache_filename);
-			LLAPRFile::remove(file_name, mHeaderAPRFilePoolp);
+            LLFile::remove(file_name);
 
 			purgeAllTextures(true);
 		}
@@ -1126,7 +1126,7 @@ void LLTextureCache::readEntriesHeader()
 {
 	// mHeaderEntriesInfo initializes to default values so safe not to read it
 	llassert_always(mHeaderAPRFile == nullptr);
-	if (LLAPRFile::isExist(mHeaderEntriesFileName, mHeaderAPRFilePoolp))
+    if (LLFile::isfile(mHeaderEntriesFileName))
 	{
 		LLAPRFile::readEx(mHeaderEntriesFileName, (U8*)&mHeaderEntriesInfo, 0, sizeof(EntriesInfo),
 						  mHeaderAPRFilePoolp);
@@ -1831,7 +1831,7 @@ void LLTextureCache::purgeTextures(bool validate)
 				std::string filename = getTextureFileName(entries[idx].mID);
 				LL_DEBUGS("TextureCache") << "Validating: " << filename << "Size: " << entries[idx].mBodySize << LL_ENDL;
 				// mHeaderAPRFilePoolp because this is under header mutex in main thread
-				S32 bodysize = LLAPRFile::size(filename, mHeaderAPRFilePoolp);
+                S32 bodysize = (S32)LLFile::size(filename);
 				if (bodysize != entries[idx].mBodySize)
 				{
 					LL_WARNS("TextureCache") << "TEXTURE CACHE BODY HAS BAD SIZE: " << bodysize << " != " << entries[idx].mBodySize << filename << LL_ENDL;
@@ -2171,7 +2171,7 @@ void LLTextureCache::openFastCache(bool first_time)
 				mFastCachePadBuffer = (U8*)ll_aligned_malloc_16(TEXTURE_FAST_CACHE_ENTRY_SIZE);
 			}
 			mFastCachePoolp = new LLVolatileAPRPool(); // is_local= true by default, so not thread safe by default
-			if (LLAPRFile::isExist(mFastCacheFileName, mFastCachePoolp))
+            if (LLFile::isfile(mFastCacheFileName))
 			{
 				mFastCachep = new LLAPRFile(mFastCacheFileName, APR_READ|APR_WRITE|APR_BINARY, mFastCachePoolp) ;				
 			}
@@ -2254,9 +2254,7 @@ void LLTextureCache::removeCachedTexture(const LLUUID& id)
 		mTexturesSizeMap.erase(id);
 	}
 	mHeaderIDMap.erase(id);
-	// We are inside header's mutex so mHeaderAPRFilePoolp is safe to use,
-	// but getLocalAPRFilePool() is not safe, it might be in use by worker
-	LLAPRFile::remove(getTextureFileName(id), mHeaderAPRFilePoolp);
+    LLFile::remove(getTextureFileName(id));
 }
 
 //called after mHeaderMutex is locked.
@@ -2269,9 +2267,7 @@ void LLTextureCache::removeEntry(S32 idx, Entry& entry, std::string& filename)
 		if (entry.mBodySize == 0)	// Always attempt to remove when mBodySize > 0.
 		{
 		  // Sanity check. Shouldn't exist when body size is 0.
-		  // We are inside header's mutex so mHeaderAPRFilePoolp is safe to use,
-		  // but getLocalAPRFilePool() is not safe, it might be in use by worker
-		  if (LLAPRFile::isExist(filename, mHeaderAPRFilePoolp))
+          if (LLFile::isfile(filename))
 		  {
 			  LL_WARNS("TextureCache") << "Entry has body size of zero but file " << filename << " exists. Deleting this file, too." << LL_ENDL;
 		  }
@@ -2291,7 +2287,7 @@ void LLTextureCache::removeEntry(S32 idx, Entry& entry, std::string& filename)
 
 	if (file_maybe_exists)
 	{
-		LLAPRFile::remove(filename, mHeaderAPRFilePoolp);		
+        LLFile::remove(filename);
 	}
 }
 

commit 7e68557536120aca38301f486b5d4a7d50c2bc7f

Fix up three defects introduced by d53e5c82c0
- LLAppViewer::removeMarkerFiles() removed mLogoutMarkerFileName in the
  branch that closes mMarkerFile, so the exec marker was never deleted on
  a clean exit and the following launch read it as a crash. The logout
  marker was removed twice instead. Restore mMarkerFileName, which is
  also what the LL_DEBUGS line immediately below already reported.

- errorMarkerExists() was defined on the same line as the closing brace
  of createErrorMarker(). Give it its own line. The function is
  deliberately kept although nothing calls it yet; a later commit in this
  series uses it.

- LLVOCache::removeFromCache() logged at LL_WARNS on every routine cache
  eviction. Lower it to LL_DEBUGS and add the missing space before
  "Filename:".

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

indra/newview/llappviewer.cpp
@@ -4247,7 +4247,7 @@ void LLAppViewer::removeMarkerFiles()
 		if (mMarkerFile.getFileHandle())
 		{
 			mMarkerFile.close() ;
-            LLFile::remove( mLogoutMarkerFileName );
+            LLFile::remove( mMarkerFileName );
 			LL_DEBUGS("MarkerFile") << "removed exec marker '"<<mMarkerFileName<<"'"<< LL_ENDL;
 		}
 		else
@@ -5642,7 +5642,9 @@ void LLAppViewer::createErrorMarker(eLastExecEvent error_code) const
             file.close();
         }
     }
-}bool LLAppViewer::errorMarkerExists() const
+}
+
+bool LLAppViewer::errorMarkerExists() const
 {
     std::string error_marker_file = gDirUtilp->getExpandedFilename(LL_PATH_LOGS, ERROR_MARKER_FILE_NAME);
     return LLFile::isfile(error_marker_file);

indra/newview/llvocache.cpp
@@ -1237,7 +1237,7 @@ void LLVOCache::removeFromCache(HeaderEntryInfo* entry)
 
 	std::string filename;
 	getObjectCacheFilename(entry->mHandle, filename);
-    LL_WARNS("VOCache") << "Removing object cache for handle " << entry->mHandle << "Filename: " << filename << LL_ENDL;
+    LL_DEBUGS("VOCache") << "Removing object cache for handle " << entry->mHandle << " Filename: " << filename << LL_ENDL;
     LLFile::remove(filename);
 	entry->mTime = INVALID_TIME ;
 	updateEntry(entry) ; //update the head file.